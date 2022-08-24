3644046115b711490f3d16dc679257d285417d9b
Vitor Natario <natariovitor@gmail.com>
2022-08-24 18:47:00 -0300

    feat: styling button with phosphor-icons

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,21 @@
+import "./styles.scss";
+import { GoogleLogo } from "phosphor-react";
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <div className="container">
+      <h1>Acesse sua conta</h1>
+
+      <span>
+        Utilizando autenticação social, por exemplo, autenticação com a Google
+        você <br /> facilita a vida do usuário permitindo utilizar sua aplicação
+        sem fazer cadastro.
+      </span>
+
+      <button type="button" className="button">
+        <GoogleLogo size={24} />
+        Entrar com o Google
+      </button>
+    </div>
+  );
 }
